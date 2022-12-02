20f6d5d63264dc354b93ab99e4a76e7c49893dbb
Jack Conger <jack.p.conger@gmail.com>
2022-12-01 20:50:49 -0800

    handle non-index 404s correctly

--- init.go
+++ init.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"log"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", http.NotFound)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Print(err)
+	}
+}
